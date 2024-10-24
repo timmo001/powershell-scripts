8a7b357d87a61b43a0bfe6fa1908af7a788c118d
Aidan Timson <aidan@timmo.dev>
2024-10-24 21:46:31 +0100

    Add prompts for hdr

---
+++ display/primary-display-prompt-hdr-disabled.ps1
@@ -0,0 +1,29 @@
+Import-Module WindowsDisplayManager
+
+$primaryDisplay = WindowsDisplayManager\GetPrimaryDisplay
+
+$enabled = $primaryDisplay.HdrInfo.HdrEnabled
+
+If ($enabled) {
+    Write-Host "HDR is enabled on the primary display."
+
+    # Prompt the user to disable HDR
+    $prompt = "Would you like to disable HDR on the primary display?"
+    $title = "Disable HDR?"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Disable HDR on the primary display."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do not disable HDR on the primary display."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    $result = $host.ui.PromptForChoice($title, $prompt, $options, 0)
+
+    If ($result -eq 0) {
+        # Disable HDR
+        $primaryDisplay.DisableHdr()
+        Write-Host "HDR has been disabled on the primary display."
+    }
+    Else {
+        Write-Host "HDR has not been disabled on the primary display."
+    }
+}
+Else {
+    Write-Host "HDR is not enabled on the primary display."
+}
---
+++ display/primary-display-prompt-hdr-enabled.ps1
@@ -0,0 +1,30 @@
+Import-Module WindowsDisplayManager
+
+$primaryDisplay = WindowsDisplayManager\GetPrimaryDisplay
+
+$enabled = $primaryDisplay.HdrInfo.HdrEnabled
+
+If ($enabled) {
+    Write-Host "HDR is enabled on the primary display."
+}
+Else {
+    Write-Host "HDR is not enabled on the primary display."
+
+    # Prompt the user to enable HDR
+    $prompt = "Would you like to enable HDR on the primary display?"
+    $title = "Enable HDR?"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Enable HDR on the primary display."
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Do not enable HDR on the primary display."
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    $result = $host.ui.PromptForChoice($title, $prompt, $options, 0)
+
+    If ($result -eq 0) {
+        # Enable HDR
+        $primaryDisplay.EnableHdr()
+        Write-Host "HDR has been enabled on the primary display."
+    }
+    Else {
+        Write-Host "HDR has not been enabled on the primary display."
+    }
+
+}
